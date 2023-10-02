3c0ad6edcc093a6d63c1300dc9a9be0a34da7c99
MadhushaPrasad <madushaprasad21@gmail.com>
2023-10-02 14:35:12 +0530

    chore(routes):corporateUser routes done

---
+++ src/routes/corporateUserRoutes.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func CorporateUserRoutes(router *gin.Engine) {
+
+	// CorporateUserRoutes
+	router.POST("/users/:user_type", nil)
+	router.PUT("/users/:user_type/:user_id", nil)
+	router.GET("/users/:user_type", nil)
+	router.GET("/users/:user_type/by_phone/:phone_number", nil)
+	router.GET("/users/:user_type/:user_id", nil)
+	router.POST("/users/:user_type/:user_id/send_verify_email", nil)
+	router.GET("/users/:user_type/:user_id/verify_email/:verification_code", nil)
+
+}

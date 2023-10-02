dc97d7ec9da5f8b1abce81272aa1243c0365b43e
MadhushaPrasad <madushaprasad21@gmail.com>
2023-10-02 15:27:28 +0530

    chore(controllers):corporateUser corporateUserController CreateUser function done

--- src/controllers/corporateUserController.go
+++ src/controllers/corporateUserController.go
@@ -1,1 +1,38 @@
 package controllers
+
+import (
+	"corporateTest/src/helpers"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"net/http"
+)
+
+func CreateUser(c *gin.Context) {
+	log := helpers.GetLogger()
+
+	log.WithFields(logrus.Fields{"ID": c.MustGet("LogID")}).Info("User Create Function Called")
+
+	userType := c.Param("user_type")
+
+	//Validate User type
+	userTypes := []string{"customer", "service_provider", "backend_user", "corporate"}
+	var userTypeValid bool
+	for _, s := range userTypes {
+		if s == userType {
+			userTypeValid = true
+			break
+		}
+		userTypeValid = false
+	}
+	if !userTypeValid {
+		c.JSON(helpers.GetHTTPError("Invalid request parameters.", http.StatusBadRequest, c.FullPath()))
+		log.WithFields(logrus.Fields{"ID": c.MustGet("LogID")}).Debug("Invalid Parameter: '" + userType + "'")
+		return
+	}
+
+	switch userType {
+	case "backend_user":
+		CreateCorporateUser(c)
+	}
+
+}

119a1e088a1b468953f3ee7db31e4adda1239927
MadhushaPrasad <madushaprasad21@gmail.com>
2023-10-03 12:31:03 +0530

    chore(main): initialize configs

--- src/main.go
+++ src/main.go
@@ -1,5 +1,44 @@
 package main
 
+import (
+	"corporateTest/src/controllers"
+	"corporateTest/src/helpers"
+	"corporateTest/src/routes"
+	"github.com/gin-gonic/gin"
+	"os"
+)
+
 func main() {
+	log := helpers.GetLogger()
+
+	controllers.InitializeControllers()
+
+	//initialize gin mode
+	ginMode, err := helpers.GetEnvStringVal("GIN_MODE")
+	if err != nil {
+		log.Error("GIN_MODE not found in environment variables")
+		os.Exit(1)
+	}
+	gin.SetMode(ginMode)
+
+	//api config port
+	port, err := helpers.GetEnvStringVal("API_CONFIG_PORT")
+	if err != nil {
+		log.Error("API_CONFIG_PORT not found in environment variables")
+		os.Exit(1)
+	}
+	if port == "" {
+		port = "8080"
+	}
+
+	router := gin.New()
+
+	//initialize routes
+	routes.CorporateUserRoutes(router)
+
+	err = router.Run(":" + port)
+	if err != nil {
+		return
+	}
 
 }

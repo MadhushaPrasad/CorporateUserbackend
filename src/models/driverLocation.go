504658dc1e2a275a5db9d3b0b085317e39857349
MadhushaPrasad <madushaprasad21@gmail.com>
2023-12-12 12:56:47 +0530

    Create driver location model

---
+++ src/models/driverLocation.go
@@ -0,0 +1,8 @@
+package models
+
+type DriverLocationUpdateModel struct {
+	PoolName  string  `json:"pool_name"`
+	DriverID  string  `json: "driver_id"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}

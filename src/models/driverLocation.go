d6d432403385b564cdc192063c81aebf6a09df7c
MadhushaPrasad <madushaprasad21@gmail.com>
2023-12-12 14:35:18 +0530

    fixed model isuue

--- src/models/driverLocation.go
+++ src/models/driverLocation.go
@@ -2,7 +2,7 @@ package models
 
 type DriverLocationUpdateModel struct {
 	PoolName  string  `json:"pool_name"`
-	DriverID  string  `json: "driver_id"`
+	DriverID  string  `json:"driver_id"`
 	Latitude  float64 `json:"latitude"`
 	Longitude float64 `json:"longitude"`
 }
